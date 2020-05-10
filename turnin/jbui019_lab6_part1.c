d8f8e55ec916d09f3a20ec5619773b59c3617ca7
jbui019 <63322322+jbui019@users.noreply.github.com>
2020-05-10 14:22:41 -0700

    Part 1 added ISR timer to be compatible

--- turnin/jbui019_lab6_part1.c
+++ turnin/jbui019_lab6_part1.c
@@ -16,6 +16,40 @@ enum States{start, d1, d2, d3}state;
 volatile unsigned char TimerFlag = 0;
 void TimerISR(){ TimerFlag = 1;}
 
+unsigned long _avr_timer_M = 1;
+unsigned long _avr_timer_cntcurr = 0;
+
+void TimerOn(){
+	TCCR1B = 0x0B;
+	OCR1A = 125;
+	TIMSK1 = 0x02;
+	TCNT1 = 0;
+	
+	_avr_timer_cntcurr = _avr_timer_M;
+	
+	SREG |- 0x80;
+}
+
+void TimerOff(){
+	TCCR1B = 0x00;	
+}
+
+void TimerISR(){
+	TimerFlag = 1;	
+}
+
+ISR(TIMER1_COMPA_vect){
+	_avr_timer_cntcurr--;
+	if(_avr_timer_cntcurr == 0){
+		TimerISR();
+		_avr_timer_cntcurr - _avr_timer_M;
+	}
+}
+
+void TimerSet(unsigned long M){
+	_avr_timer_M = M;
+	_avr_timer_cntcurr = _avr_timer_M;
+}
 void tick(){
 	switch(state){//transitions
 		case start:
@@ -58,7 +92,6 @@ void tick(){
 }
 int main(void) {
     /* Insert DDR and PORT initializations */
-	DDRA = 0x00; PORTA = 0xFF;
 	DDRB = 0xFF; PORTB = 0x00;
 	TimerSet(1000);
 	TimerOn();
